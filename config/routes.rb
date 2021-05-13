1fe9c948649d57e39c5284e5046be6ec0858d11a
EC2 Default User <rihoyan01@gmail.com>
2021-05-13 19:35:09 +0000

    devise作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,20 @@ Rails.application.routes.draw do
     passwords: 'admins/passwords',
     registrations: 'admins/registrations'
   }
-  
+
   devise_for :salons, controllers: {
     sessions: 'salons/sessions',
     passwords: 'salons/passwords',
     registrations: 'salons/registrations'
   }
-  
+
   devise_for :customers, controllers: {
     sessions: 'customers/sessions',
     passwords: 'customers/passwords',
     registrations: 'customers/registrations'
   }
-  
+
+  root to: 'home/top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
